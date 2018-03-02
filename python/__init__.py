fd863d0206175985141e7e4768814e97065b4aea
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2018-03-02 14:37:09 +0100

    [python] Organize as python package

--- python/__init__.py
+++ python/__init__.py
@@ -13,7 +13,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 
-from _eos import _Parameters
+from ._eos import _Parameters
 
 class Parameters(_Parameters):
     @staticmethod
@@ -46,4 +46,4 @@ class Parameters(_Parameters):
 
         return parameters
 
-from _eos import *
+from ._eos import *
